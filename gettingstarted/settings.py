c66515b3dd1cfc0e9029d21cab99172c06df5dfb
Robert <robert.pastor0691@orange.fr>
2022-10-04 21:39:38 +0200

    settings

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'NAME': 'musicrungis',
         'USER': 'mysql',
         'PASSWORD': 'Bobby1&&&xxx',
-        'HOST': 'localhost',
+        'HOST': 'RobertPastor.mysql.pythonanywhere-services.com',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
